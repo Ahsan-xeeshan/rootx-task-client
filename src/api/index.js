95bd42b93643ae7ba3672ab7e710c95df30aabaf
Xeeshan <xeeshann.ahsan@gmail.com>
2025-08-30 20:40:16 +0600

    new render

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   import.meta.env.MODE === "development"
     ? "http://localhost:8080"
-    : "https://rootx-task-backend.onrender.com";
+    : "https://rootx-task-manager.onrender.com";
 
 const api = axios.create({
   baseURL,
